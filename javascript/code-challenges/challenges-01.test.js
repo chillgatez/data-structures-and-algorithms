272d3e7637e608f8ca85d5320dfe85e40c9af584
Kelsee Watts <123975076+chillgatez@users.noreply.github.com>
2023-04-17 19:38:03 -0500

    challenge 4 completed

--- javascript/code-challenges/challenges-01.test.js
+++ javascript/code-challenges/challenges-01.test.js
@@ -68,10 +68,16 @@ Use `forEach` to build a new array of strings, each string modified by the callb
 
 const greeting = (word) => {
   // Solution code here...
+  return `${word.toUpperCase()}!`
 };
 
 const speaker = (words, callback) => {
   // Solution code here...
+  let arrSpeech = [];
+  words.forEach(value => {
+    arrSpeech.push(callback(value));
+  });
+  return arrSpeech;
 };
 
 /* ------------------------------------------------------------------------------------------------
